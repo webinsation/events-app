5e12c14d01dc5c74aabff9f48b1274c75096127b
Caleb Mellas <caleb@webinsation.com>
2014-08-21 07:48:35 -0500

    added function to create a new event and add it to the list

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 var event = angular.module("EventApp", []);
 
 event.controller("EventCtrl", ['$scope','$http', function($scope, $http) {    
-        $http.get('data/events.json').success (function(data){
-            $scope.events = data.events;
-        });
-    }]
-);
-event.directive('addEvent', function() {
-	$scope.events.push({
-		title:$scope.eventFormTitle,
-		date:$scope.eventFormDate,
-		location:$scope.eventFormLocation,
-		attendees:$scope.eventFormAttendees
-	});
-});
+    $http.get('data/events.json').success (function(data){
+        $scope.events = data.events;
+    });
+
+    $scope.createEvent = function(event) {
+    	var defaultForm = {
+    		title: "",
+    		date: "",
+    		location: ""
+    	};
+    	$scope.events.push({
+    		title: event.title,
+    		date: event.date,
+    		location: event.location
+    	});
+    	$scope.event = defaultForm; 
+    };
+}]);
